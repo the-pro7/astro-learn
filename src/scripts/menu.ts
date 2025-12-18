d73bdb94f532827e3b267bb019c59318a8b2ec10
the-pro7 <ameyawemmanuel900@gmail.com>
2025-12-18 02:14:13 +0000

    Implement responsive menu with toggle functionality and refactor related components

---
+++ src/scripts/menu.ts
@@ -0,0 +1,6 @@
+const menu = document.querySelector('.menu') as HTMLButtonElement;
+
+menu?.addEventListener('click', () => {
+  const isExpanded = menu.getAttribute('aria-expanded') === 'true';
+  menu.setAttribute('aria-expanded', `${!isExpanded}`);
+});
